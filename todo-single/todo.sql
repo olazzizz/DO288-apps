c355474a895722ff2dd3590aded6391578c0946b
root <root@occam-inf.fyre.ibm.com>
2020-11-29 08:52:38 -0800

    Added tye SQL file for deployment lifecycle

---
+++ todo-single/todo.sql
@@ -0,0 +1,4 @@
+insert into Item (id, description, done) values (1, 'Execute the pipeline on staging environment', 0);
+insert into Item (id, description, done) values (2, 'Review the CI/CD processes', 0);
+insert into Item (id, description, done) values (3, 'Read a relaxing book', 0);
+
