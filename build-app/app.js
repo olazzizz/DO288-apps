d5e1aeca4b5f188eab3c3115a18b1af32fd4c374
olazzizz <user@email.com>
2025-04-30 18:58:59 +0200

    You go

--- build-app/app.js
+++ build-app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('OK, I changed the message here again!\n');
+  res.send('OK, I changed the message here again 2 T!\n');
 });
 
 app.listen(8080, function () {
-  console.log('OK, I changed the message here again!');
+  console.log('OK, I changed the message here again 2 T!');
 });
 
